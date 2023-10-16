4d8ea60500afa689fbd49dc6da463bb28ee09151
LucaFasone <luca.fasone15@gmail.com>
2023-10-16 02:51:19 +0200

    styling

--- src/Components/ListBox.jsx
+++ src/Components/ListBox.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { Listbox } from '@headlessui/react'
+import Style from './CompoentesStyles.module.css'
 
 function ListBox() {
-  
 const options = [
   { id: 1, name: 'Scegli un opzione' },
   { id: 2, name: 'Vedi la lista dei messaggi' },
@@ -11,15 +11,20 @@ const options = [
   const [selectedOption, setSelectedOption] = useState(options[0])
 
   return (
-    <div className="mt-4 w-full relative">
+    <div className="mt-4 w-full sm:w-72">
       <Listbox value={selectedOption} onChange={setSelectedOption} >
-      <Listbox.Button  className="cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">{selectedOption.name}</Listbox.Button>
-      <Listbox.Options>
+      <Listbox.Button  className={Style.ListBox}>{selectedOption.name}</Listbox.Button>
+      <Listbox.Options className={Style.ListBox}>
         {options.map((option) => (
           <Listbox.Option
+          className={({ active }) =>
+              `${Style.ListBoxOption} ${
+                active? Style.ListBoxOptionActive : ''
+              }`}
             key={option.id}
             value={option}>
             {option.name}
+            
           </Listbox.Option>
         ))}
       </Listbox.Options>
